27fe2be1e5caea4d14f5741ed037a3bc638c0913
3367497332 <3367497332@qq.com>
2020-04-28 12:33:50 +0800

    修改文件Student.java 添加sname属性

--- Student.java
+++ Student.java
@@ -1,12 +1,21 @@
 public class Student{
 
 	private Integer sid;
+	private String sname;
 
 	public void setSid(Integer sid){
 		this.sid = sid;
 	}
 
+	public void setSname(String sname){
+		this.sname = sname;
+	}
+
 	public Integer getSid(){
 		return sid;
+	}
+
+	public String getSname(){
+		return sname;
 	}
 }
